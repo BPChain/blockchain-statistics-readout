beb549ab70feed219754bd2892cdb7592c5f1b43
Anton von Weltzien <anton.weltzien@gmail.com>
2018-05-28 11:28:59 +0200

    Move functionality into super adapter.

--- statistics_reader/blockchain_adapter.py
+++ statistics_reader/blockchain_adapter.py
@@ -7,6 +7,7 @@ class BlockchainAdapter:
 
     def __init__(self, is_miner):
         self.is_miner = is_miner
+        self.previous_block_number = 0
 
     def hashrate(self) -> int:
         raise NotImplementedError()
@@ -18,4 +19,19 @@ class BlockchainAdapter:
         raise NotImplementedError()
 
     def new_blocks_and_previous(self) -> Tuple[List[Block], Block]:
+        newest_block_number = self.fetch_newest_block_number()
+        raw_blocks = [self.fetch_block_with(number)
+                       for number in range(self.previous_block_number, newest_block_number + 1)]
+        blocks = [self.make_block_from(raw_block) for raw_block in raw_blocks]
+        old_block = blocks[0]
+        new_blocks = blocks[1:] if len(blocks) > 1 else []
+        return new_blocks, old_block
+
+    def fetch_newest_block_number(self) -> int:
+        raise NotImplementedError()
+
+    def fetch_block_with(self, number: int):
+        raise NotImplementedError()
+
+    def make_block_from(self, raw_block) -> Block:
         raise NotImplementedError()
