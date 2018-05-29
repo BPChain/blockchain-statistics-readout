9ab8a629eb7037d03b73c9081a0686d77c461a63
Anton von Weltzien <anton.weltzien@gmail.com>
2018-05-29 11:38:27 +0200

    remove unneeded dep for ws

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='statistics_reader',
-      version='0.9.1',
+      version='0.9.2',
       description='Read out statistics from a blockchain',
       author='Anton von Weltzien',
       license='MIT',
       packages=['statistics_reader'],
       url='https://github.com/BPChain/blockchain_statistics_readout.git',
-      install_requires=['websocket-client==0.47.0', 'psutil==5.4.5', 'requests==2.18.4'],
+      install_requires=['psutil==5.4.5', 'requests==2.18.4'],
       zip_safe=False)
