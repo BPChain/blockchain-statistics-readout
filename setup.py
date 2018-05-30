81538b08ae55805e759395670f62326aac6b4996
Anton von Weltzien <anton.weltzien@gmail.com>
2018-05-30 11:12:18 +0200

    Clean up sender interface

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='statistics_reader',
-      version='0.10',
+      version='1',
       description='Read out statistics from a blockchain',
       author='Anton von Weltzien',
       license='MIT',
--- statistics_reader/sender.py
+++ statistics_reader/sender.py
@@ -2,20 +2,23 @@ from time import sleep
 
 import requests as http_client
 
-from .blockchain_reader import BlockchainReader
+from .blockchain_reader import BlockchainAdapter, BlockchainReader
 from .logger import logger_for
 
 LOGGER = logger_for(__name__)
 
 
 class Sender:
-    def __init__(self, server_address, period, reader: BlockchainReader):
+    def __init__(self, server_address: str, process_name: str, chain_name: str,
+                 adapter: BlockchainAdapter, period=15):
         """
-        :param server_address: the address of the server e.g. http://myserver.com/route
-        :param period: wait period between data sends in seconds
-        :param reader: a reader initialized with a blockchain adapter
+        :param server_address: Where to send the data such as "http://myserver.com/route"
+        :param process_name: name of the blockchain process
+        :param chain_name: Name of blockchain
+        :param adapter: An adapter for the blockchain, subclassing BlockchainAdapter
+        :param period: How often in seconds data is send
         """
-        self.reader = reader
+        self.reader = BlockchainReader(process_name, chain_name, adapter)
         self.send_data_every(period, server_address)
 
     def send_data_every(self, period, server_address):
@@ -27,4 +30,4 @@ class Sender:
                 response = http_client.post(server_address, json=json_data)
                 LOGGER.info('Sent data, received %s from server', response.status_code)
             except Exception as exception:
-                LOGGER.warning("Exception occurred during sending: '%s'", exception)
+                LOGGER.exception("Exception occurred during sending: '%s'", exception)
